350cdad4ff97357c978d2b756fe9dbca3cbcd6e9
Yashwanthsai97 <yashwanthsaisrirama94@gmail.com>
2026-04-03 12:28:52 +0530

    Revert "Migrate user storage from JSON to MySQL"
    This reverts commit 49c99d7b6da0a7f71bbf303b94f7c0c09e540165.
    
    # Conflicts:
    #	.env.example
    #	README.md
    #	config/db.py
    #	config/settings.py
    
    # Conflicts:
    #	README.md

--- routes/auth_routes.py
+++
@@ -1,6 +0,0 @@
-from controllers.auth_controller import login, signup
-
-
-def register_auth_routes(app):
-    app.add_url_rule("/api/auth/signup", endpoint="signup", view_func=signup, methods=["POST"])
-    app.add_url_rule("/api/auth/login", endpoint="login", view_func=login, methods=["POST"])
--- routes/profile_routes.py
+++
@@ -1,5 +0,0 @@
-from controllers.profile_controller import save_profile
-
-
-def register_profile_routes(app):
-    app.add_url_rule("/api/profile", endpoint="update_profile", view_func=save_profile, methods=["POST"])
